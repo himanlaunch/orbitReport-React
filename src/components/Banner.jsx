c5ceff54bae0792b183c7dc7f96bbef59d31098f
himanlaunch <himanshureddyk1995@gmail.com>
2024-08-29 19:19:13 -0500

    .

---
+++ src/components/Banner.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./styling.css";
+
+const Banner = () => (
+  <>
+    <header>Orbit Report</header>
+    <p>Click on the buttons to see the satellites in that orbit type.</p>
+  </>
+);
+
+export default Banner;
